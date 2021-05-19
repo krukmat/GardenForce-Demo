1536fe2098b4219314c28028626b4269ab98ee0a
Matias Kruk <kruk.matias@gmail.com>
2021-05-19 13:56:25 +0200

    bugfix

--- python/mqtt_daemon.py
+++ python/mqtt_daemon.py
@@ -2,5 +2,5 @@
 import sys, os
 sys.path.append('/rest /var/www/html/GardenForce-Demo/python/')
 import mqtt_proxy
-client = connect_mqtt()
+client = mqtt_proxy.connect_mqtt()
 client.loop_forever()
