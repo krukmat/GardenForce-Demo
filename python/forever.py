da380a6070616e821332e1e54eb7765a37dedf73
Matias Kruk <kruk.matias@gmail.com>
2021-05-19 13:54:58 +0200

    forover

--- python/forever.py
+++ python/forever.py
@@ -2,7 +2,7 @@
 from subprocess import Popen
 import sys
 
-filename = './mqtt_proxy.py'
+filename = './mqtt_daemon.py'
 while True:
     print("\nStarting " + filename)
     p = Popen("python3 " + filename, shell=True)
